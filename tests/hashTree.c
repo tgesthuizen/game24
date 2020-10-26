90c15faa571ee95f65708eee55fc60fa20d212c0
Tim Gesthuizen <tim.gesthuizen@yahoo.de>
2020-10-26 22:16:43 +0100

    tests: Add test searching for hash collisions

--- tests/hashTree.c
+++ tests/hashTree.c
@@ -21,7 +21,42 @@ static void checkCallback(const SyntaxTree x, const struct Node *root,
   checkSameTreeAlwaysHashesTheSame(x);
 }
 
+enum { ring_buffer_size = 64 };
+
+struct ring_buffer {
+  size_t filled;
+  uint16_t data[ring_buffer_size];
+};
+
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
+
+static void checkGeneratedTreesDontHashTheSameCallback(const SyntaxTree x,
+                                                       const struct Node *root,
+                                                       void *data) {
+  (void)root;
+  const uint16_t hash = hashTree(x);
+  struct ring_buffer *buffer = data;
+  for (uint16_t *cur = buffer->data,
+                *end = cur + MIN(ring_buffer_size, buffer->filled);
+       cur != end; ++cur) {
+    if (*cur == hash) {
+      printf("%s: %d: Two trees hashes clash. Their hashes are %d and %d. The "
+             "latter is ",
+             __FILE__, __LINE__, (int)*cur, (int)hash);
+      printFullTree(x);
+      putchar('\n');
+    }
+  }
+  memmove(buffer->data + 1, buffer->data,
+          MIN(buffer->filled, ring_buffer_size - 1));
+  *buffer->data = hash;
+  buffer->filled = MIN(buffer->filled + 1, ring_buffer_size);
+}
+
 int main() {
   int nums[number_count] = {1, 2, 3, 4};
   iterateAllSyntaxTrees(nums, &checkCallback, NULL);
+  struct ring_buffer buffer = (struct ring_buffer){.filled = 0, .data = {0}};
+  iterateAllSyntaxTrees(nums, &checkGeneratedTreesDontHashTheSameCallback,
+                        &buffer);
 }
