760cafa65f7ff30019258617b70be38039d3681c
Tim Gesthuizen <tim.gesthuizen@yahoo.de>
2020-10-27 20:25:22 +0100

    tests: Add tests for swap
    * tests/CMakeLists.txt (CHECK_PROG): Add swap program.
    * tests/swap.c: New file.

---
+++ tests/swap.c
@@ -0,0 +1,46 @@
+#define main xmain
+#include "../game24.c"
+
+#undef main
+
+int main() {
+  {
+    int i = 1, j = 2;
+    swap(&i, &j);
+    if (j != 1 || i != 2) {
+      printf("%s: %d: Expected i=2, j=1, but found them to be i=%d, j=%d\n",
+             __FILE__, __LINE__, i, j);
+    }
+  }
+  {
+    int i = 1;
+    swap(&i, &i);
+    if (i != 1) {
+      printf(
+          "%s: %d: Expected i to stay unchanged after self swap, but found it "
+          "to be %d",
+          __FILE__, __LINE__, i);
+    }
+  }
+  {
+    struct ABitOfEverything {
+      int a;
+      short b;
+      float c;
+      long d;
+    } i = {1, 2, 3, 4}, j = {5, 6, 7, 8};
+    const struct ABitOfEverything k = i, l = j;
+    swap(&i, &j);
+#define CHECK_STRUCT_EQ(found, expected)                                       \
+  if (found.a != expected.a || found.b != expected.b ||                        \
+      found.c != expected.c || found.d != expected.d) {                        \
+    printf("%s: %d: Expected i and l to be equivalent bout they are not: "     \
+           "{%d, %d, %f, %d} vs. {%d, %d, %f, %d}\n",                          \
+           __FILE__, __LINE__, (int)found.a, (int)found.b, (float)found.c,     \
+           (int)found.d, (int)expected.a, (int)expected.b, (float)expected.c,  \
+           (int)expected.d);                                                   \
+  }
+    CHECK_STRUCT_EQ(i, l);
+    CHECK_STRUCT_EQ(j, k);
+  }
+}
