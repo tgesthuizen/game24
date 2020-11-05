cdc0945eef4eb5db444a37d4eb8bb343e9b9037d
Tim Gesthuizen <tim.gesthuizen@yahoo.de>
2020-11-05 11:46:17 +0100

    Backport changes from the second iteration to the first
    * iteration1.c: Move changes from game24.c that also apply to
      iteration1.c.

--- iteration1.c
+++ iteration1.c
@@ -1,6 +1,39 @@
+#include <assert.h>
 #include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#ifdef __GNUC__
+#define NORETURN __attribute__((noreturn))
+#define CANT_REACH __builtin_unreachable();
+#else
+#define NORETURN
+#define CANT_REACH
+#endif
+
+static NORETURN void handleOutOfMemory() {
+  fputs("System is out of memory, aborting", stderr);
+  abort();
+}
+
+static void *xmalloc(size_t size) {
+  void *ptr = malloc(size);
+  if (ptr) {
+    return ptr;
+  }
+  handleOutOfMemory();
+}
+
+static void *xrealloc(void *ptr, size_t size) {
+  ptr = realloc(ptr, size);
+  if (ptr) {
+    return ptr;
+  }
+  handleOutOfMemory();
+}
 
 enum {
   number_count = 4,
@@ -12,18 +45,18 @@ enum OperatorKind { op_add, op_sub, op_mul, op_div };
 
 struct Operator {
   enum OperatorKind kind;
-  char lhs, rhs;
+  unsigned char lhs, rhs;
 };
 enum NodeKind { node_number, node_operator };
 struct Node {
   enum NodeKind kind;
-  union {
+  union NodeValue {
     int n;
     struct Operator op;
   } v;
 };
 
-typedef struct Node SyntaxTree[7];
+typedef struct Node SyntaxTree[all_count];
 typedef struct {
   int num;
   bool valid;
@@ -55,16 +88,18 @@ static EvalResult evalSyntaxTree(const SyntaxTree tree,
     case op_mul:
       return makeNumber(lhs.num * rhs.num);
     case op_div:
-      return rhs.num != 0 ? makeNumber(lhs.num / rhs.num) : makeInvalid();
+      return (rhs.num != 0 && lhs.num % rhs.num == 0)
+                 ? makeNumber(lhs.num / rhs.num)
+                 : makeInvalid();
     }
   }
   }
-  __builtin_unreachable();
+  CANT_REACH
 }
 
+static const char opChars[4] = {'+', '-', '*', '/'};
 static void printSyntaxTreeImpl(const SyntaxTree tree,
                                 const struct Node *curNode) {
-  static const char opChars[4] = {'+', '-', '*', '/'};
   switch (curNode->kind) {
   case node_number:
     printf("%d", curNode->v.n);
@@ -82,40 +117,42 @@ static void printSyntaxTree(const SyntaxTree tree, const struct Node *curNode) {
   putchar('\n');
 }
 
-static void incrementOperators(enum OperatorKind ops[3]) {
-  ++ops[0];
-  if (ops[0] != op_div + 1)
-    return;
-  ops[0] = op_add;
-  ++ops[1];
-  if (ops[1] != op_div + 1)
-    return;
-  ops[1] = op_add;
-  ++ops[2];
-  if (ops[2] != op_div + 1)
-    return;
-  ops[2] = op_add;
+static void incrementOperators(enum OperatorKind ops[ops_count]) {
+  for (size_t i = 0; i < ops_count; ++i) {
+    ++ops[i];
+    if (ops[i] != op_div + 1) {
+      return;
+    }
+    ops[i] = op_add;
+  }
 }
 
-static void swap(char *a, char *b) {
-  char c = *a;
-  *a = *b;
-  *b = c;
+static void swap_impl(void *a, void *b, void *restrict c, size_t size) {
+  memcpy(c, a, size);
+  memmove(a, b, size);
+  memcpy(b, c, size);
 }
+#define swap(a, b)                                                             \
+  swap_impl(                                                                   \
+      (a), (b),                                                                \
+      (char[sizeof(*(a)) == sizeof(*(b)) ? (ptrdiff_t)sizeof(*(a)) : -1]){0},  \
+      sizeof(*(a)))
 
-static bool compareOps(const enum OperatorKind lhs[ops_count],
-                       const enum OperatorKind rhs[ops_count]) {
+static int compareOps(const enum OperatorKind lhs[ops_count],
+                      const enum OperatorKind rhs[ops_count]) {
   for (int i = 0; i < ops_count; ++i) {
     if (lhs[i] != rhs[i]) {
-      return false;
+      return 0;
     }
   }
-  return true;
+  return 1;
 }
 
 static void iterateAllSyntaxTrees(const int numbers[4],
                                   void (*callback)(const SyntaxTree tree,
-                                                   const struct Node *root)) {
+                                                   const struct Node *root,
+                                                   void *data),
+                                  void *data) {
   SyntaxTree tree;
   static const enum OperatorKind finalOps[ops_count] = {op_div, op_div, op_div};
   enum OperatorKind ops[ops_count] = {op_add, op_add, op_add};
@@ -124,19 +161,20 @@ static void iterateAllSyntaxTrees(const int numbers[4],
 #define FOR_VAR(name, top) for (int name = 0; name < top; ++name)
 #define FOR_OPERAND(name, top)                                                 \
   FOR_VAR(name##_lhs, top) FOR_VAR(name##_rhs, top - 1)
-    FOR_OPERAND(first, 4) FOR_OPERAND(second, 3) FOR_OPERAND(third, 2) {
+    FOR_OPERAND(first, number_count)
+    FOR_OPERAND(second, number_count - 1) FOR_OPERAND(third, number_count - 2) {
       // Setup the tree
-      for (int i = 0; i < 4; ++i) {
+      for (int i = 0; i < number_count; ++i) {
         tree[i] = (struct Node){.kind = node_number, {.n = numbers[i]}};
       }
-      for (int i = 0; i < 3; ++i) {
+      for (int i = 0; i < ops_count; ++i) {
         tree[i + 4] =
             (struct Node){.kind = node_operator, {.op = {ops[i], -1, -1}}};
       }
-      char itab[7] = {0, 1, 2, 3, 4, 5, 6};
-      int arenaRight = 4;
-      int curNode = 4;
-      struct Node *curOperator = tree + 4;
+      char itab[all_count] = {0, 1, 2, 3, 4, 5, 6};
+      int arenaRight = number_count;
+      int curNode = number_count;
+      struct Node *curOperator = tree + number_count;
       curOperator->v.op.lhs = itab[first_lhs];
       swap(itab + first_lhs, itab + --arenaRight);
       curOperator->v.op.rhs = itab[first_rhs];
@@ -152,18 +190,17 @@ static void iterateAllSyntaxTrees(const int numbers[4],
       curOperator->v.op.rhs = itab[third_rhs];
       swap(itab + third_rhs, itab + curNode++);
       ++curOperator;
-      callback(tree, tree + 6);
+      callback(tree, tree + all_count - 1, data);
     }
   }
 }
 
-static int count = 0;
 static void checkAndPrintCallback(const SyntaxTree tree,
-                                  const struct Node *root) {
+                                  const struct Node *root, void *data) {
   const EvalResult res = evalSyntaxTree(tree, root);
   if (res.valid && res.num == 24) {
     printSyntaxTree(tree, root);
-    ++count;
+    ++*((int *) data);
   }
 }
 
@@ -176,9 +213,10 @@ int main() {
       return 1;
     }
   }
-  iterateAllSyntaxTrees(numbers, checkAndPrintCallback);
+  int count = 0;
+  iterateAllSyntaxTrees(numbers, checkAndPrintCallback, &count);
   if (!count) {
-    puts("No solutions!\n");
+    puts("No solutions!");
   }
   return 0;
 }
